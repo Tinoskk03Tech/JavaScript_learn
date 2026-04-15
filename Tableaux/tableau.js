797d2008aef73e420ec5b0d86e2d1ddbcd67849f
Tinoskk03Tech <kossivitinek@gmail.com>
2026-04-15 23:45:49 +0000

    Envoie de mon code js sur les tableaux

---
+++ Tableaux/tableau.js
@@ -0,0 +1,14 @@
+var joursDeLaSemaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+var monTableau = [12, "Dosseh", "Vignon", ["a", "b", "c"], true, false]
+
+console.log(monTableau)
+
+monTableau[2] = "JavaScriptLab";
+monTableau[6] = "Voiture";
+
+monTableau[monTableau.length] = "Avion";
+
+console.log(monTableau)
+
+var monDeuxiemeTableau = Array();
